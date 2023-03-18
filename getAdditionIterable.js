33ca00d6d95f4f1b638a025bec984da9756f7bc2
Manuel-Smith <riddicksmith37@gmail.com>
2023-03-18 21:57:08 +0300

    Renamed the get_additionalIterable file to getAdditonIterable

---
+++ getAdditionIterable.js
@@ -0,0 +1,25 @@
+function getAdditionIterable() {
+//   let currentPosition = start;
+
+//   let additionIterable = {
+//     next() {
+//       let result = {};
+//       if (currentPosition < end) {
+//         result.value = currentPosition;
+//         result.done = false;
+
+//         currentPosition += step;
+//       } else {
+//         result.done = true;
+//       }
+
+//       return result;
+//     },
+//   };
+
+return true;
+}
+
+
+
+getAdditionIterable();
